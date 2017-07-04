309e25ded6720ec04b833ac8ebcf64f3a5667d35
kasthurikaliappan <kasthuridivya97@gmail.com>
2017-07-04 14:34:42 +0530

    Add files via upload

---
+++ Reversenumber.java
@@ -0,0 +1,18 @@
+package kas;
+import java.util.*;
+public class Reversenumber {
+	public static void main(String args[]){
+		int n,rev=0,rem;
+		Scanner s=new Scanner(System.in);
+		System.out.println("enter a number");
+		n=s.nextInt();
+		while(n!=0){
+			rem=n%10;
+			rev=rev*10+rem;
+			n=n/10;
+			
+		}
+		System.out.println("reverse ="+rev);
+	}
+
+}
